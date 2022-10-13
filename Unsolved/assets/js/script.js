7f793edcaf8004a17803361b5a7551e3315c2ace
Tom Bond <tombond248@gmail.com>
2022-10-13 21:10:00 +0100

    complete

--- Unsolved/assets/js/script.js
+++ Unsolved/assets/js/script.js
@@ -4,4 +4,9 @@ container.addEventListener("click", function(event) {
   var element = event.target;
 
   // TODO: Complete function
+
+  const dataNumber = element.getAttribute('data-number')
+  element.textContent = dataNumber
+
+
 });
